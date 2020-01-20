eb8d3d561d69b6f799b70839333539eee34dea76
MiloHwb <hwbmilo@gmail.com>
2020-01-20 11:32:37 +0800

    频道页下拉刷新

--- lib/page/main_page/channel_page.dart
+++ lib/page/main_page/channel_page.dart
@@ -10,51 +10,57 @@ import 'package:xbilibili/providers/channel_page_provider.dart';
  * @ 描述         
  */
 class ChannelPage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     getChannelList(context);
-    return Consumer<ChannelPageProvider>(builder: (context, value, child) {
-      if (value.hasError) {
-        return Center(
-          child: OutlineButton(
-            onPressed: () {
-              getChannelList(context);
-            },
-            child: Text('请点击重试'),
-            borderSide: BorderSide(
-              color: Theme.of(context).primaryColor,
-              width: 1,
+    return RefreshIndicator(
+      onRefresh: (){
+        return getChannelList(context);
+      },
+      child: Consumer<ChannelPageProvider>(builder: (context, value, child) {
+        if (value.hasError) {
+          return Center(
+            child: OutlineButton(
+              onPressed: () {
+                getChannelList(context);
+              },
+              child: Text('请点击重试'),
+              borderSide: BorderSide(
+                color: Theme.of(context).primaryColor,
+                width: 1,
+              ),
+              textColor: Theme.of(context).primaryColor,
             ),
-            textColor: Theme.of(context).primaryColor,
-          ),
-        );
-      } else {
-        return GridView.count(
-          crossAxisCount: 4,
-          shrinkWrap: true,
-          children: value.region.map((region) {
-            return Column(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(top: 10),
-                  child: Image.network(
-                    region.logo,
-                    width: MediaQuery.of(context).size.width / 10,
+          );
+        } else {
+          return GridView.count(
+            crossAxisCount: 4,
+            shrinkWrap: true,
+            children: value.region.map((region) {
+              return Column(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: Image.network(
+                      region.logo,
+                      width: MediaQuery.of(context).size.width / 10,
+                    ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 10),
-                  child: Text(region.name),
-                ),
-              ],
-            );
-          }).toList(),
-        );
-      }
-    });
+                  Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: Text(region.name),
+                  ),
+                ],
+              );
+            }).toList(),
+          );
+        }
+      }),
+    );
   }
 
-  void getChannelList(BuildContext context) {
+  Future<Null> getChannelList(BuildContext context) async{
     HttpMethod.getChannelList().then((value) {
       Provider.of<ChannelPageProvider>(context, listen: false).setRegions(value.data.region);
     }).catchError((e) {
