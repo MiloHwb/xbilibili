1b0f12195074c35627c67a71101b0be6a23728ac
MiloHwb <hwbmilo@gmail.com>
2020-01-26 20:56:49 +0800

    追番完成

--- lib/page/main_page/home_page/bangumi_page.dart
+++ lib/page/main_page/home_page/bangumi_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:provider/provider.dart';
 import 'package:xbilibili/entity/bangumi_model.dart';
 import 'package:xbilibili/providers/bangumi_page_provider.dart';
+import 'package:xbilibili/route/routes.dart';
+
+import '../../../r.dart';
 
 /*
  * @ 创建者       milohuang
@@ -19,7 +23,6 @@ class BangumiPage extends StatelessWidget {
         return Future.delayed(Duration(seconds: 1));
       },
       child: Container(
-        margin: EdgeInsets.all(10),
         child: Consumer<BangumiProvider>(builder: (context, value, child) {
           return ListView.builder(
             itemCount: value.list.length,
@@ -27,6 +30,12 @@ class BangumiPage extends StatelessWidget {
               Widget child;
               if (value.list[index] is BangumiBanners) {
                 child = _buildBanners(value.list[index]);
+              } else if (value.list[index] is BangumiRegions) {
+                child = _buildRegion(value.list[index]);
+              } else if (value.list[index] is String) {
+                child = _buildLogin(context);
+              } else if (value.list[index] is BangumiTile) {
+                child = _buildTile(value.list[index], context);
               }
               return child;
             },
@@ -36,9 +45,113 @@ class BangumiPage extends StatelessWidget {
     );
   }
 
+  Widget _buildTile(BangumiTile bangumiTile, context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(bangumiTile.title),
+                Text(
+                  '查看更多',
+                  style: TextStyle(color: Theme.of(context).primaryColor),
+                )
+              ],
+            ),
+          ),
+          GridView.count(
+            shrinkWrap: true,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 5,
+            childAspectRatio: 1.1,
+            physics: NeverScrollableScrollPhysics(),
+            crossAxisCount: 2,
+            children: bangumiTile.list.map((tile) {
+              return _buildTileItem(tile);
+            }).toList(),
+          ),
+          SizedBox(height: 10),
+          Text(
+            '换一换',
+            style: TextStyle(color: Theme.of(context).primaryColor),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _buildTileItem(BangumiItem tile) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Stack(
+              children: <Widget>[
+                Positioned.fill(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                    child: Image.network(
+                      tile.cover,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Text(tile.title),
+          Text(
+            tile.desc,
+            style: TextStyle(color: Colors.grey, fontSize: 12),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLogin(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed(RouteName.loginPage);
+      },
+      child: Image.asset(R.imagesBangumiHomeLoginGuide),
+    );
+  }
+
+  Widget _buildRegion(BangumiRegions regions) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: Colors.grey[400], width: 0.5),
+        ),
+      ),
+      padding: EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: regions.regions.map((region) {
+          return Tab(
+            icon: Image.network(
+              region.icon,
+              width: 35,
+            ),
+            text: region.title,
+          );
+        }).toList(),
+      ),
+    );
+  }
+
   Widget _buildBanners(BangumiBanners banners) {
     return Container(
       height: 190,
+      margin: EdgeInsets.all(10),
       child: Swiper(
         itemCount: banners.regions.length,
         autoplay: true,
